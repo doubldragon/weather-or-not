f712316aeb95c772b1f42c97952250ea9fe91177
brandon <brandon.spencer@gmail.com>
2019-08-26 22:54:01 -0400

    basic styling on login/register pages

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/js/index.jsx')
+    .addEntry('main', './assets/scss/main.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -54,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
